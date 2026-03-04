4a1f0ffc631a51d4cfecf5470dab800d70e2c6f5
Justinkho29 <justindavekho29@gmail.com>
2026-03-04 20:58:43 +0800

    Refactor event listeners to use arrow functions

--- lava.js
+++ lava.js
@@ -1,6 +1,5 @@
 function showSurvey(question) {
     const response = prompt(question);
-
     if (response) {
         alert('Salamat sa iyong sagot: ' + response);
     } else {
@@ -11,21 +10,16 @@ function showSurvey(question) {
 document.addEventListener('DOMContentLoaded', function() {
     const contactBtn = document.getElementById('nav-contact');
     if (contactBtn) {
-        contactBtn.addEventListener('click', function() {
-            showSurvey('SURVEY: Ano ang iyong katanungan para sa "Contact Us"?');
-        });
+        contactBtn.addEventListener('click', () => showSurvey('SURVEY: Ano ang iyong katanungan para sa "Contact Us"?'));
     }
-
+    
     const aboutBtn = document.getElementById('nav-about');
     if (aboutBtn) {
-        aboutBtn.addEventListener('click', function() {
-            showSurvey('SURVEY: Ano ang pinaka-gusto mong malaman tungkol sa amin?');
-        });
+        aboutBtn.addEventListener('click', () => showSurvey('SURVEY: Ano ang pinaka-gusto mong malaman tungkol sa amin?'));
     }
-
+    
     const emailBtn = document.getElementById('nav-email');
     if (emailBtn) {
-        emailBtn.addEventListener('click', function() {
-            showSurvey('SURVEY: Paki-lagay ang iyong email para sa aming newsletter.');
-        });
-    
+        emailBtn.addEventListener('click', () => showSurvey('SURVEY: Paki-lagay ang iyong email para sa aming newsletter.'));
+    }
+});
