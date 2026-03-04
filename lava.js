57b431aefdb9ee7c664e0c3813f01b8b15084325
Justinkho29 <justindavekho29@gmail.com>
2026-03-04 20:46:54 +0800

    Refactor event listeners to use function syntax

--- lava.js
+++ lava.js
@@ -1,5 +1,6 @@
 function showSurvey(question) {
     const response = prompt(question);
+
     if (response) {
         alert('Salamat sa iyong sagot: ' + response);
     } else {
@@ -10,16 +11,21 @@ function showSurvey(question) {
 document.addEventListener('DOMContentLoaded', function() {
     const contactBtn = document.getElementById('nav-contact');
     if (contactBtn) {
-        contactBtn.addEventListener('click', () => showSurvey('SURVEY: Ano ang iyong katanungan para sa "Contact Us"?'));
+        contactBtn.addEventListener('click', function() {
+            showSurvey('SURVEY: Ano ang iyong katanungan para sa "Contact Us"?');
+        });
     }
-    
+
     const aboutBtn = document.getElementById('nav-about');
     if (aboutBtn) {
-        aboutBtn.addEventListener('click', () => showSurvey('SURVEY: Ano ang pinaka-gusto mong malaman tungkol sa amin?'));
+        aboutBtn.addEventListener('click', function() {
+            showSurvey('SURVEY: Ano ang pinaka-gusto mong malaman tungkol sa amin?');
+        });
     }
-    
+
     const emailBtn = document.getElementById('nav-email');
     if (emailBtn) {
-        emailBtn.addEventListener('click', () => showSurvey('SURVEY: Paki-lagay ang iyong email para sa aming newsletter.'));
-    }
-}); 
+        emailBtn.addEventListener('click', function() {
+            showSurvey('SURVEY: Paki-lagay ang iyong email para sa aming newsletter.');
+        });
+    
