2456228344819cd6baf9b92e4f0d2c62add1d7db
Justinkho29 <justindavekho29@gmail.com>
2026-03-21 21:24:43 +0800

    Update main.js with new survey function and navigation code

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-   
-        
-        function showSurvey(question) {
-            const response = prompt(question);
-            if (response) {
-                alert('Salamat sa iyong sagot: ' + response);
-            } else {
-                alert('Nag-cancel ka ng survey.');
-            }
-        }
+function showSurvey(question) {
+    const response = prompt(question);
+    if (response) {
+        window.location.href = 'index.html';
+    } else {
+        window.location.href = 'index.html';
+    }
+}
 
-        document.getElementById('nav-contact').addEventListener('click', function() {
-            showSurvey('SURVEY: Ano ang iyong katanungan para sa "Contact Us"?');
-        });
+document.getElementById('nav-contact').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
 
-        document.getElementById('nav-about').addEventListener('click', function() {
-            showSurvey('SURVEY: Ano ang pinaka-gusto mong malaman tungkol sa amin?');
-        });
+document.getElementById('nav-about').addEventListener('click', function() {
+    window.location.href = 'last.html';
+});
 
-        document.getElementById('nav-email').addEventListener('click', function() {
-            showSurvey('SURVEY: Paki-lagay ang iyong email para sa aming newsletter.');
-        });
-
-        
+document.getElementById('nav-email').addEventListener('click', function() {
+    window.location.href = 'protein.html';
+});
